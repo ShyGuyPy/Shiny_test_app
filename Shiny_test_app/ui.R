d0c6cf0d35159f1ee6bcee30851fe1b3580865de
Luke Vawter <luke.vawter1@gmail.com>
2018-08-22 15:03:38 -0400

    initial commit shiny test app

---
+++ Shiny_test_app/ui.R
@@ -0,0 +1,11 @@
+shinyUI(
+  navbarPage(title = "shiny_test",
+    id = "tab",
+    inverse = TRUE,
+    theme =shinythemes::shinytheme("cyborg"),
+    source("ui/test_slider.R", local = TRUE)$value,
+    source("ui/tets_thingy.R", local = TRUE)$value
+    
+
+  )
+)
---
+++ Shiny_test_app/ui/test_slider.R
@@ -0,0 +1,3 @@
+sliderInput(inputId = "value to change", 
+            label = "test_slider", 
+            value = 10, min = 1, max = 50)

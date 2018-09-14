014cfe4aef1ed33507496e8ec7924bdbc578ca08
ShyGuyPy <luke.vawter1@gmail.com>
2018-09-14 11:59:51 -0400

    created space for working on DREX css

--- Shiny_test_app/ui.R
+++ Shiny_test_app/ui.R
@@ -6,8 +6,9 @@ shinyUI(
     source("ui/test_slider.R", local = TRUE)$value,
     source("ui/test_panel.R", local = TRUE)$value,
     #source("ui/test_date_range.R", local = TRUE)$value,
-    source("ui/test_rawHTML.R", local = TRUE)$value
+    source("ui/test_rawHTML.R", local = TRUE)$value,
     #source("ui/test_box.R", local = TRUE)$value
+    source("ui/DREX1_panel.R", local = TRUE)$value
 
   )
 )
---
+++ Shiny_test_app/ui/DREX1_panel.R
@@ -0,0 +1,1 @@
+tabPanel("Drex_toy")

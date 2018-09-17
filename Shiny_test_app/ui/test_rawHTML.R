93d54fb573f9383122ccee3cc3bc7cc76b4cceab
Luke Vawter <lvawter@icprb.org>
2018-09-17 12:39:38 -0400

    added rough quadrant structure to test 2018DREX elements

--- Shiny_test_app/ui/test_rawHTML.R
+++ Shiny_test_app/ui/test_rawHTML.R
@@ -1,9 +1,62 @@
-tabPanel("HTML",
+tabPanel("quad",
          withTags(
-         div(class = "header",
-                  p("Some text"),
-                  p("some more text")
-         )
-)
+           html(
+            body(
+           div(class="square",
+               div(class="content",
+                   div(class="table",
+                       div(class="table-cell",
+                           p("some text")
+                       )))),
+           div(class="square",
+               div(class="content",
+                   div(class="table",
+                       div(class="table-cell",
+                           p("some text 2")
+                       )))),
+           div(class="square",
+               div(class="content",
+                   div(class="table",
+                       div(class="table-cell",
+                           p("some text 3")
+                       )))),
+           div(class="square",
+               div(class="content",
+                   div(class="table",
+                       div(class="table-cell",
+                           p("some text 4")
+                       ))))
+         
+         
+         ))
+         ))
 
-)
+
+
+
+
+# tabPanel("quadrants",
+#          withTags(
+#            div(class="square",
+#                div(class="content",
+#                    div(class="table",
+#                        div(class="table-cell",
+#                            p("some text")
+#            ))))
+# 
+# 
+#          ))
+
+
+
+
+# <div class="square">
+#   <div class="content">
+#     <div class="table">
+#       <div class="table-cell">
+#         ... CONTENT HERE ...
+#       </div>
+#         </div>
+#         </div>
+#         </div>
+#         ... and so on 9 times for 9 squares ...
